f04a6f23d07c521daafd26eff526a195ebc4e83f
therealstezzy <97630660+therealstezzy@users.noreply.github.com>
2022-05-01 22:30:59 -0700

    saving code

---
+++ skillsDemoTester.java
@@ -0,0 +1,10 @@
+
+import static org.junit.Assert.*;
+import org.junit.*;
+public class skillsDemoTester {
+    @Test
+    public void onePlusTest() {
+        assertEquals(2, skillsDemo.plusOne(1));
+    }
+}
+
